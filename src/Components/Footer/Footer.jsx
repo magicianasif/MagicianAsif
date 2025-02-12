8491c57f67128fe408589ef122e9cb4cc018f3df
magicianasif <asifhabeeb1205@gmail.com>
2025-02-12 14:26:22 +0530

    updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import user_icon from '../../assets/user_icon.svg'
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className="footer-top">
+      {/* <div className="footer-top">
         <div className="footer-top-left">
             <img src={footer_logo} alt="" />
             <p>I am a frontend developer from, USA with 10 years of experience in companies like Microsoft, Tesla and Apple.</p>
@@ -18,10 +18,10 @@ const Footer = () => {
             </div>
             <div className="footer-subscribe">Subscribe</div>
         </div>
-      </div>
+      </div> */}
       <hr />
       <div className="footer-bottom">
-        <p className="footer-bottom-left">© 2023 Alex Bennett. All rights reserved.</p>
+        <p className="footer-bottom-left">© 2023 Magician Asif. All rights reserved.</p>
         <div className="footer-bottom-right">
             <p>Term of Services</p>
             <p>Privacy Policy</p>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profileHero.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
+  const handleDownload = () => {
+    const resumeUrl = "/AsifHabeeb_Resume_.pdf"; // Ensure the file is inside the public folder
+    const link = document.createElement("a");
+    link.href = resumeUrl;
+    link.download = "Asif_Habeeb_Resume.pdf"; // File name for download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
-    <div id='home' className='hero'>
-      <img src={profile_img} alt="" className='profile-img' />
-      <h1><span>I'm Asif Habeeb,</span> frontend developer cum close up magician based in India.</h1>
-      <p>I am a frontend developer from Kerala, India with 3+ years of experience in multiple companies like Cubet Techno Labs and Kalkitech.</p>
+    <div id="home" className="hero">
+      <img src={profile_img} alt="" className="profile-img" />
+      <h1>
+        <span>I'm Asif Habeeb,</span> frontend developer cum close up magician
+        based in India.
+      </h1>
+      <p>
+        I am a frontend developer from Kerala, India with 3+ years of experience
+        in multiple companies like Cubet Techno Labs and Kalkitech.
+      </p>
       <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
+        <button onClick={handleDownload} className="hero-resume">
+          My resume
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
