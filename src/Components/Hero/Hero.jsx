f25482b242b6280197c3c8e0e9f067648580d120
magicianasif <asifhabeeb1205@gmail.com>
2025-02-12 13:14:17 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+      <img src={profile_img} alt="" className='profile-img' />
+      <h1><span>I'm Asif Habeeb,</span> frontend developer cum close up magician based in India.</h1>
+      <p>I am a frontend developer from Kerala, India with 3+ years of experience in multiple companies like Cubet Techno Labs and Kalkitech.</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
